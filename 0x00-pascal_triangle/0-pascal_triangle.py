70fe9ddf9ca11deba8b465428bfa812f290172d3
chigivera <ayman.benchamkha@gmail.com>
2025-06-19 03:24:46 +0100

    Refactor pascal_triangle function to improve implementation and add detailed docstring. The new version calculates Pascal's triangle using combinatorial logic and handles edge cases more effectively.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
+"""
+0. Pascal's Triangle
+"""
+
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing Pascal's triangle of n."""
-    if n <= 0:
-        return []
-    
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        current_row = [1]
-        
-        for j in range(1, i):
-            current_row.append(prev_row[j-1] + prev_row[j])
-        
-        current_row.append(1)
-        triangle.append(current_row)
-    
-    return triangle
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
